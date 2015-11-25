52d061af918f23bede5146c518ccd0fa20210a33
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-11-25 13:43:39 -0800

    Add LoginViewController

---
+++ Feedings/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  Feedings
+//
+//  Created by Kevin McGladdery on 11/25/15.
+//  Copyright © 2015 Kevin McGladdery. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+}
