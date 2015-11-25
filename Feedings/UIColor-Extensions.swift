6cb49002a108ae51971e2590f5e3687e575182cd
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-11-25 14:45:56 -0800

    Add colors to UIColor extension

---
+++ Feedings/UIColor-Extensions.swift
@@ -0,0 +1,21 @@
+//
+//  UIColor-Extensions.swift
+//  Feedings
+//
+//  Created by Kevin McGladdery on 11/25/15.
+//  Copyright © 2015 Kevin McGladdery. All rights reserved.
+//
+
+import UIKit
+import Colortools
+
+extension UIColor {
+  
+  static public var feedingsOrange: UIColor {
+    return UIColor.init(hex: 0xF86400FF)
+  }
+  
+  static public var feedingsBlue: UIColor {
+    return UIColor.init(hex: 0x00BCF8FF)
+  }
+}
