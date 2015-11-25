0c2a6f87d83ce7cb0708a3880e085d37eff26747
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-11-24 22:37:29 -0800

    Add FeedingsListViewController

---
+++ Feedings/FeedingsListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  FeedingsListViewController.swift
+//  Feedings
+//
+//  Created by Kevin McGladdery on 11/24/15.
+//  Copyright © 2015 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FeedingsListViewController: UIViewController {
+
+}
