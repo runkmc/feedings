880258fb1d3db26cc3e248a17b315fc3b2d3f83c
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-11-25 14:46:08 -0800

    Update to new UIButton subclass

--- Feedings/LoginViewController.swift
+++ Feedings/LoginViewController.swift
@@ -7,7 +7,21 @@
 //
 
 import UIKit
+import Bond
+import Colortools
+import Parse
 
 class LoginViewController: UIViewController {
 
+  @IBOutlet weak var usernameField: UITextField!
+  @IBOutlet weak var passwordField: UITextField!
+  @IBOutlet weak var loginButton: UIButton!
+  @IBOutlet weak var signupButton: UIButton!
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    loginButton.layer.cornerRadius = 24
+    signupButton.layer.cornerRadius = 24
+  }
 }
