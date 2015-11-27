657265ba7902b1633b24a080525416f1b7c5121b
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-11-27 12:20:11 -0800

    get working logout button

---
+++ Feedings/SettingsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SettingsViewController.swift
+//  Feedings
+//
+//  Created by Kevin McGladdery on 11/27/15.
+//  Copyright © 2015 Kevin McGladdery. All rights reserved.
+//
+
+import UIKit
+import Colortools
+import Parse
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func logoutTapped(sender: AnyObject) {
+        PFUser.logOutInBackground()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
