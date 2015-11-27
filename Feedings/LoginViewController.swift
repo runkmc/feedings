1405c4c55c891263f22dd7bf8cd1e5f730a8436a
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-11-27 11:14:48 -0800

    Implement login logic

--- Feedings/LoginViewController.swift
+++ Feedings/LoginViewController.swift
@@ -13,22 +13,36 @@ import Parse
 
 class LoginViewController: UIViewController {
 
-  @IBOutlet weak var usernameField: UITextField!
-  @IBOutlet weak var passwordField: UITextField!
-  @IBOutlet weak var loginButton: UIButton!
-  @IBOutlet weak var signupButton: UIButton!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signupButton: UIButton!
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
   
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    loginButton.layer.cornerRadius = 24
-    signupButton.layer.cornerRadius = 24
-    
-    usernameField.underline(UIColor.init(hex: 0xD5D5D5FF))
-    passwordField.underline(UIColor.init(hex: 0xD5D5D5FF))
-  }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loginButton.layer.cornerRadius = 24
+        signupButton.layer.cornerRadius = 24
+        
+        usernameField.underline(UIColor.init(hex: 0xD5D5D5FF))
+        passwordField.underline(UIColor.init(hex: 0xD5D5D5FF))
+    }
     
     @IBAction func loginTapped(sender: AnyObject) {
+        PFUser.logInWithUsernameInBackground(usernameField.text!, password: passwordField.text!) {
+            (user: PFUser?, error: NSError?) -> Void in
+            if let _ = user {
+                self.dismissViewControllerAnimated(true, completion: nil)
+            }
+            if let loginError = error {
+                let errorString = loginError.userInfo["error"] as? String
+                let alert = UIAlertController.init(title: "Problem with Login", message: errorString, preferredStyle: .Alert)
+                let action = UIAlertAction.init(title: "Ok", style: .Default, handler:nil)
+                alert.addAction(action)
+                self.presentViewController(alert, animated: true, completion: nil)
+            }
+                
+        }
     }
     
 }
