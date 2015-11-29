750e8286bc2a91768c68b6822499dddd7036f53b
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-11-29 13:50:03 -0800

    Add AddFeedingViewController and view

---
+++ Feedings/AddFeedingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddFeedingViewController.swift
+//  Feedings
+//
+//  Created by Kevin McGladdery on 11/29/15.
+//  Copyright © 2015 Kevin McGladdery. All rights reserved.
+//
+
+import UIKit
+
+class AddFeedingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
