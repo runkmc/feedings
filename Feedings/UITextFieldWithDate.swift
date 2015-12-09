0a6b782470da97d9db41781ac65927c3463b5c86
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-12-08 18:40:01 -0800

    Create UITextFieldWithDate subclass

---
+++ Feedings/UITextFieldWithDate.swift
@@ -0,0 +1,14 @@
+//
+//  UITextFieldWithDate.swift
+//  Feedings
+//
+//  Created by Kevin McGladdery on 12/8/15.
+//  Copyright © 2015 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UITextFieldWithDate: UITextField {
+    var date: NSDate?
+}
