1af13655c9f2db87545cfc209ba01ec572a84342
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-12-12 16:53:22 -0800

    Create Day class

---
+++ Feedings/Day.swift
@@ -0,0 +1,14 @@
+//
+//  Day.swift
+//  Feedings
+//
+//  Created by Kevin McGladdery on 12/11/15.
+//  Copyright © 2015 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
+import Parse
+
+class Day {
+    
+}
