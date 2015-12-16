88a82155dcdbb5a9319c2dcc710744807ffa8c2d
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-12-15 18:23:26 -0800

    Change tests to reflect new classes

--- FeedingsTests/DaySpec.swift
+++ FeedingsTests/DaySpec.swift
@@ -9,6 +9,7 @@
 import Foundation
 import Quick
 import Nimble
+import Bond
 import Parse
 @testable import Feedings
 
@@ -18,21 +19,21 @@ class DaySpec: QuickSpec {
             let day = self.setupDay()
             
             it("returns the day's calorie count") {
-                expect(day.calories) == "645"
+                expect(day.calories.value) == "645"
             }
             
             it("returns the day's volume amount") {
-                expect(day.volume) == "770"
+                expect(day.volume.value) == "770"
             }
             
             it("returns 0 calories when there are no feedings yet") {
                 let emptyDay = Day(feedings: [])
-                expect(emptyDay.calories) == "0"
+                expect(emptyDay.calories.value) == "0"
             }
             
             it("returns 0 ml when there are no feedings yet") {
                 let emptyDay = Day(feedings: [])
-                expect(emptyDay.volume) == "0"
+                expect(emptyDay.volume.value) == "0"
             }
         }
     }
